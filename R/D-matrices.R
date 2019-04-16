5bcf1623c1329b0407c394ee6e10e7671403685d
Yadi <shahryary@gmail.com>
2019-04-16 17:40:53 +0200

    Updated

--- R/D-matrices.R
+++ R/D-matrices.R
@@ -8,10 +8,12 @@
 #' @param directory Path to the data-source directory
 #' @param cytosines Type of cytosines (CHG or CG or CHH)
 #' @param posteriorMaxFilter Filter value, based on posteriorMax ex: >= 0.95 or 0.99
+#' @param genTable Generation table name, you can find sample file in "extdata" generations.fn
 #' @return A matrix of the infile
 #' @export
 #'
-dMatrices<-function(directory, cytosines, posteriorMaxFilter ){
+
+dMatrices<-function(directory, cytosines, posteriorMaxFilter, genTable ){
   # checking errors
   if (!dir.exists(directory)){
     stop("Directory of data-source not exists!")
@@ -23,15 +25,18 @@ dMatrices<-function(directory, cytosines, posteriorMaxFilter ){
     stop("Please enter the valid Cytosines. (CHH or CHG or CG)")
   }
   # check if posteriorMaxFiltervalue is correct
-  posteriorMaxFilter <- as.numeric(posteriorMaxFilter)
-  if (posteriorMaxFilter < 0 | posteriorMaxFilter > 1 ){
+  pMFilter <- as.numeric(posteriorMaxFilter)
+  if (pMFilter < 0 | pMFilter > 1 ){
     stop("posteriorMax value must be < 1 and > 0. ")
   }
-  data.names<-list.files(paste0(dirname(directory),"/",basename(directory)),pattern = "*.txt", full.names = TRUE)
+  data.names <- list.files(paste0(dirname(directory),"/",basename(directory)),pattern = "*.txt|*.tsv", full.names = TRUE)
+  gen_tbl <- fread(genTable)
+  if (length(data.names)!= NROW(gen_tbl)){
+    stop("Number of file is not matching with data file generation names. ")
+  }
 
-  ds_pairs<-combn(data.names, 2)
-  print(ds_pairs)
-  final_ds <- runMatrix((length(ds_pairs)/2), cytosines, pMFilter)
+  pairs<-combn(data.names, 2)
+  final_ds <- runMatrix(pairs, cytosines, pMFilter, genTable)
   print("Writing to the files.")
   colnames(final_ds)<-(c("pair-1","pair-2","D-value","cytosines","filter"))
   fwrite(final_ds,file = paste0(getwd(),"/","d-matrices-",cytosines,"-",pMFilter,".csv") , quote = FALSE, sep = '\t', row.names = FALSE, col.names = TRUE)
@@ -41,23 +46,29 @@ dMatrices<-function(directory, cytosines, posteriorMaxFilter ){
 # -------------------------------------------------------------
 # getting file names
 #-------------------------------------------------------------
-getNames <- function(nameA ){
-  tmp_name_A<-gsub(pattern = "\\.*[^G^0-9.L-]", "", basename(nameA))
-  A=gsub(pattern = "\\-.*", "",tmp_name_A)
-  AA<- gsub(pattern = "\\.*[^G0-9]", "-L", basename(A))
-  return(AA)
+getNames <- function(genTable, nameDF){
+  gen_tbl <- fread(genTable)
+  strSearch<-basename(nameDF)
+  tmp_A <- gen_tbl[gen_tbl$filename == strSearch,][,2]
+  tmp_B <- gen_tbl[gen_tbl$filename == strSearch,][,3]
+  if (tmp_B == ""){
+  gen_name <- paste0(tmp_A,tmp_B)
+  }else{
+    gen_name <- paste0(tmp_A,"-",tmp_B)
+  }
+  return(gen_name)
 }
-runMatrix <- function(pairs, cytosines, pMFilter){
+runMatrix <- function(pairs, cytosines, pMFilter, genTable){
   flag=TRUE
-  for (i in 1:pairs){ #(length(ds_pairs)/2)
-      in_file <-ds_pairs[,i]
-      name_ds<- getNames(in_file[1])
-      name_ds[2]<- getNames(in_file[2])
-      cat(paste0("Running: ",name_ds[[1]], " and ",name_ds[[2]], " ( ", i , " out of ",length(ds_pairs)/2," )"),"\n")
+  for (i in 1:(length(pairs)/2)){
+      df <-pairs[,i]
+      name_ds<- getNames(genTable, df[1])
+      name_ds[2]<- getNames(genTable, df[2])
+      cat(paste0("Running: ",name_ds[[1]], " and ",name_ds[[2]], " ( ", i , " out of ",length(pairs)/2," )"),"\n")
 
       cat("Constructing data-frame ...\n")
-      file_A <- fread(in_file[1], skip = 0, sep = '\t',select=c("seqnames","start","strand","context","posteriorMax","status"))
-      file_B <- fread(in_file[2], skip = 0, sep = '\t',select=c("seqnames","start","strand","context","posteriorMax","status"))
+      file_A <- fread(df[1], skip = 0, sep = '\t',select=c("seqnames","start","strand","context","posteriorMax","status"))
+      file_B <- fread(df[2], skip = 0, sep = '\t',select=c("seqnames","start","strand","context","posteriorMax","status"))
       # filter out based on context & posteriorMax
       file_A <- file_A %>% filter(context == cytosines & posteriorMax >= pMFilter )
       file_B <- file_B %>% filter(context == cytosines & posteriorMax >= pMFilter )
@@ -102,7 +113,7 @@ runMatrix <- function(pairs, cytosines, pMFilter){
       rm(tmp_db)
     }
     cat(paste0(name_ds[[1]], " and ",name_ds[[2]], " is done! \n"))
-    cat("-----------------------------------")
+    cat("-----------------------------------\n")
     name_ds<-NULL
   }
   return(tmp_big)
